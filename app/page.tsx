e201a6cfed158bc551f6fa470d3730cb0ca8207c
Niraj Nepal <niraj.nepal70@gmail.com>
2024-08-22 16:45:46 -0400

    commit 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,170 @@
-import Image from "next/image";
+'use client';
+import Link from 'next/link';
+import { FaChevronDown } from "react-icons/fa6";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaReact, FaNodeJs, FaGithub } from "react-icons/fa";
+import { DiMongodb } from "react-icons/di";
+import { SiExpress } from "react-icons/si";
+import { TbBrandSocketIo } from "react-icons/tb";
+import { Chrono } from "react-chrono";
 
-export default function Home() {
+import React from 'react';
+
+export default function HomePage() {
+
+  const events = [ 
+    {
+      title: "2019",
+      cardTitle: "Web Developer",
+      cardSubtitle: "Web Developer",
+      cardDetailedText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        }
+      }
+    },
+    {
+      title: "2020",
+      cardTitle: "Web Developer",
+      cardSubtitle: "Web Developer",
+      cardDetailedText: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl. Sed euismod, nunc ut aliquam aliquam, nunc nisl aliquam nisl, eget aliquam nisl nisl sit amet nisl.",
+      media: {
+        type: "IMAGE",
+        source: {
+          url: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+        }
+      }
+    },
+    {
+      title: "2021",
+      cardTitle: "Web Developer",
+
+    }
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="bg-black container mx-auto px-4">
+      <nav>
+        <div className='container mx-auto flex justify-between items-center py-4 px-4'>
+            <div className='text-xl text-white font-bold'>
+              <Link href='/'>
+                N i r a j <span className='text-green-500'>.</span> 
+              </Link>
+            </div>
+
+          {/* <ul className="flex text-white text-md space-x-6"> 
+            <li className='hover:text-green-500'>Home</li>
+            <li className='hover:text-green-500'>Service</li>
+            <li className='hover:text-green-500'>Contact </li>
+          </ul> */}
         </div>
-      </div>
+      </nav>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section className='py-32'>
+        <div className='container mx-auto text-center mb-20'>
+          <h1 className='text-4xl font-bold mb-8'>
+            Hi👋 I'm Niraj!
+          </h1>
+          <p className='text-lg'>Software Engineer </p>
+          <p>Check below to learn more about me!</p>
+        </div>
+        <div className='flex justify-center  text-green-500'><FaChevronDown size={40} className='hover:text-white' /></div>
+      </section>
+    
+      <section className="py-30">
+        <h1 className='flex justify-center mb-10 text-3xl font-bold'>Check out my Highlighted Projects</h1>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="container mx-auto">
+          <div className="flex flex-col m-3 sm:m-0 justify-center md:flex-row md:justify-between gap-4 text-black">
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="bg-black border-2 text-white border-green-500 shadow-md rounded-lg p-6 w-full md:w-1/3 flex flex-col justify-center items-center text-center">
+              <h2 className="text-2xl font-bold mb-4">VerseNetwork</h2>
+              <p className="text-white  mb-4">A platform designed to foster community engagement among gamers.</p>
+              <p className="mb-4 ">Skills Utilized</p>
+              <div className="flex justify-center items-center space-x-4 text-green-500  mb-5"> 
+                <IoLogoJavascript /> 
+                <FaReact /> 
+                <FaNodeJs />
+                <SiExpress />
+                <DiMongodb />
+              </div>
+                <a href="https://github.com/niraj1298/Verse-Network" target="_blank" rel="noopener noreferrer" className="text-white hover:text-green-500 transition duration-300">
+                      View on GitHub
+                </a>
+           </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+           <div className="bg-black border-2 text-white border-green-500 shadow-md rounded-lg p-6 w-full md:w-1/3 flex flex-col justify-center items-center text-center">
+              <h2 className="text-2xl font-bold mb-4">Secure Chatting App</h2>
+              <p className="text-white  mb-4">A Full-stack web app built for E2E Ecrypted messaging.</p>
+              <p className="mb-4">Skills Utilized</p>
+              <div className="flex justify-center items-center text-green-500 space-x-4 mb-5"> 
+                <IoLogoJavascript /> 
+                <FaReact /> 
+                <FaNodeJs />
+                <SiExpress />
+                <TbBrandSocketIo />
+              </div>
+                <a href="https://github.com/niraj1298/Verse-Network" target="_blank" rel="noopener noreferrer" className="text-white hover:text-green-500 transition duration-300">
+                      View on GitHub
+                </a>
+           </div>
+
+           <div className="bg-black border-2 text-white border-green-500 shadow-md rounded-lg p-6 w-full md:w-1/3 flex flex-col justify-center items-center text-center">
+              <h2 className="text-2xl font-bold mb-4">Python Detection</h2>
+              <p className="text-white  mb-4">A Full-stack web app built for E2E Ecrypted messaging.</p>
+              <p className="mb-4">Skills Utilized</p>
+              <div className="flex justify-center items-center text-green-500 space-x-4 mb-5"> 
+                <IoLogoJavascript /> 
+                <FaReact /> 
+                <FaNodeJs />
+                <SiExpress />
+                <TbBrandSocketIo />
+              </div>
+                <a href="https://github.com/niraj1298/Verse-Network" target="_blank" rel="noopener noreferrer" className="text-white hover:text-green-500 transition duration-300">
+                      View on GitHub
+                </a>
+           </div>
+           
+          </div>
+        </div>
+      </section>
+      
+      <section className='my-40'>
+        <div className='container mx-auto'>
+          <div className='flex flex-col items-center'>
+            <h1 className='text-3xl font-bold mb-4'>Work Experience</h1>
+            <Chrono items={events}  mode="VERTICAL_ALTERNATING" disableToolbar 
+            theme={{
+              primary: '#22C55E',
+              secondary: 'black',
+              cardBgColor: 'white',
+              titleColor: 'white',
+              titleColorActive: '#22C55E',
+            }}
+            />
+
+          </div>
+        </div>
+</section>
+
+      <footer className='bg-black text-white py-20'>
+        <div className='container mx-auto flex flex-col justify-center items-center space-y-4'>
+          <p className='text-lg'>© 2024 Niraj. All rights reserved.</p>
+          <div className='flex space-x-4'>
+            <a href="https://github.com/niraj1298" target="_blank" rel="noopener noreferrer" className="text-white hover:text-green-500 transition duration-300">GitHub</a>
+            <a href="https://linkedin.com/in/niraj1298" target="_blank" rel="noopener noreferrer" className="text-white hover:text-green-500 transition duration-300">LinkedIn</a>
+            <a href="https://twitter.com/niraj1298" target="_blank" rel="noopener noreferrer" className="text-white hover:text-green-500 transition duration-300">Twitter</a>
+            
+          </div>
+          <button className='bg-green-500 text-black px-4 py-2 rounded hover:bg-green-700 transition duration-300'>
+            <a href="#contact">Contact Me</a>
+          </button>
+        </div>
+      </footer>
+
+    </div>
   );
 }
