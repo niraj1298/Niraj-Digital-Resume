d636725f229b6e71d420a6ef690593d9b05de075
Niraj Nepal <niraj.nepal70@gmail.com>
2024-08-24 14:09:20 -0400

    Added components, and many other features.

--- app/page.tsx
+++ app/page.tsx
@@ -1,145 +1,21 @@
 'use client';
-import Link from 'next/link';
-import { IoLogoJavascript } from "react-icons/io";
-import { FaReact, FaNodeJs } from "react-icons/fa";
-import { DiMongodb } from "react-icons/di";
-import { SiExpress } from "react-icons/si";
-import { TbBrandSocketIo } from "react-icons/tb";
-import Image from 'next/image';
+import NavBar from '../components/NavBar';
+import Hero from '../components/Hero';
+import About from '../components/About';
+import Projects from '../components/Projects';
+import Experience from '../components/Experience';
+import Footer from '../components/Footer';
 
 export default function HomePage() {
   return (
     <div className="bg-white text-gray-900">
-
-      {/* Container for centered content */}
       <div className="max-w-7xl mx-auto px-4">
-        {/* Navigation */}
-        <nav className="py-4 border-b border-gray-200">
-          <div className="flex justify-between items-center">
-            <div className="text-2xl font-bold">
-              <Link href='/'>
-                N i r a j <span className="text-blue-500">.</span> 
-              </Link>
-            </div>
-          </div>
-        </nav>
-
-        {/* Hero Section */}
-        <section className="min-h-screen py-32 flex flex-col items-center md:flex-row">
-          <div className="md:w-1/2 text-left">
-            <h1 className="text-5xl font-extrabold mb-10">Hi👋 I'm Niraj</h1>
-            <p className="text-xl leading-relaxed text-gray-700">
-              I'm a <span className="text-blue-500">Software Engineer</span> <br /> with a passion for <span className="text-blue-500">cutting-edge technology</span>, <br /> a knack for <span className="text-blue-500">problem-solving</span>, and a drive to
-              <span className="text-blue-500"> innovate</span> <br /> and adapt in any situation.{" "}
-            </p>
-            <p className="mt-10 text-lg text-blue-500">Click below to connect!</p>
-            <div className="flex justify-start mt-5">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">Connect</button>
-            </div>
-          </div>
-          <div className="md:w-1/2 flex justify-center mt-10 md:mt-0">
-            <div className="relative w-[500px] h-[500px] md:w-80 md:h-80 rounded-full overflow-hidden shadow-lg">
-              <Image
-                src="/first.png"
-                alt="Niraj"
-                layout="fill"
-                objectFit="contain"
-              />
-            </div>
-
-
-          </div>
-        </section>
-
-        {/* About Me Section */}
-        <section className="py-20 bg-gray-100 text-gray-900 rounded-lg shadow-lg mb-20">
-          <div className="max-w-4xl mx-auto">
-            <h1 className="text-4xl font-extrabold text-center mb-10">About Me</h1>
-            <div className="flex flex-col md:flex-row items-center gap-8">
-              <div className="md:w-1/2 flex justify-center items-center">
-                <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden shadow-lg">
-                  <Image src="/H1.png" alt="Your Name" layout="fill" objectFit="contain" />
-                </div>
-              </div>
-              <div className="md:w-1/2">
-                <div className="bg-white p-6 rounded-lg shadow-md mx-4 md:mx-0">
-                  <p className="text-lg leading-relaxed mb-4">
-                    👨🏽 <span className="font-bold">Here's a bit more about me!</span>
-                  </p>
-                  <ul className="list-inside space-y-4">
-                    <li>🎓 Recent Graduate in Computer Science & Cyber Security with a Minor in Mathematics.</li>
-                    <li>📍 Currently working as an Analyst in a 1-year tech rotation program @ PNC.</li>
-                    <li>🥋 Passionate about giving back through martial arts. I volunteer at a nonprofit to teach karate to kids in my community, focusing on their mental, physical, and spiritual well-being.</li>
-                    <li>✨ <span className="font-bold">Interests:</span> AI, Cybersecurity, Cloud Computing, Embedded Systems, Game, Mobile Development, and Robotics.</li>
-                    <li>🚀 Always looking for new challenges and opportunities to make an impact. Let's connect!</li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
-
-        {/* Projects Section */}
-        <section className="py-30">
-          <h1 className="text-3xl font-bold text-center mb-10">Highlighted Projects</h1>
-          <div className="flex flex-col md:flex-row justify-between gap-4">
-            {[
-              {
-                title: "VerseNetwork",
-                description: "A platform designed to foster community engagement among gamers.",
-                skills: [<IoLogoJavascript />, <FaReact />, <FaNodeJs />, <SiExpress />, <DiMongodb />],
-                link: "https://github.com/niraj1298/Verse-Network"
-              },
-              {
-                title: "Secure Chatting App",
-                description: "A Full-stack web app built for E2E Encrypted messaging.",
-                skills: [<IoLogoJavascript />, <FaReact />, <FaNodeJs />, <SiExpress />, <TbBrandSocketIo />],
-                link: "https://github.com/niraj1298/Verse-Network"
-              },
-              {
-                title: "Python Detection",
-                description: "A Python-based detection system for various use cases.",
-                skills: [<IoLogoJavascript />, <FaReact />, <FaNodeJs />, <SiExpress />, <TbBrandSocketIo />],
-                link: "https://github.com/niraj1298/Verse-Network"
-              }
-            ].map((project, index) => (
-              <div key={index} className="bg-white border-2 text-gray-900 border-gray-200 shadow-md rounded-lg p-6 w-full md:w-1/3 flex flex-col justify-center items-center text-center">
-                <h2 className="text-2xl font-bold mb-4">{project.title}</h2>
-                <p className="text-gray-700 mb-4">{project.description}</p>
-                <p className="mb-4">Skills Utilized</p>
-                <div className="flex justify-center items-center text-blue-500 space-x-4 mb-5"> 
-                  {project.skills.map((skill, idx) => <span key={idx}>{skill}</span>)}
-                </div>
-                <a href={project.link} target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-blue-600 transition duration-300">
-                  View on GitHub
-                </a>
-              </div>
-            ))}
-          </div>
-        </section>
-
-        {/* Work Experience Section */}
-        <section className="my-40 text-center">
-          <h1 className="text-3xl font-bold mb-4">Work Experience</h1>
-        </section>
-
-        {/* Footer */}
-        <footer className="bg-gray-100 text-gray-900 py-20">
-          <div className="max-w-screen-xl mx-auto flex flex-col justify-center items-center space-y-4">
-            <p className="text-lg">© 2024 Niraj. All rights reserved.</p>
-            <div className="flex space-x-4">
-              <a href="https://github.com/niraj1298" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500 transition duration-300">GitHub</a>
-              <a href="https://linkedin.com/in/niraj1298" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500 transition duration-300">LinkedIn</a>
-              <a href="https://twitter.com/niraj1298" target="_blank" rel="noopener noreferrer" className="hover:text-blue-500 transition duration-300">Twitter</a>
-            </div>
-            <Link href="#contact">
-              <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">
-                Contact Me
-              </button>
-            </Link>
-          </div>
-        </footer>
+      <NavBar />
+      <Hero />
+      <About />
+      <Projects />
+      <Experience />
+      <Footer />
       </div>
     </div>
   );
---
+++ components/Experience.jsx
@@ -0,0 +1,176 @@
+'use client';
+import React, { useState } from 'react';
+import { IoLogoJavascript } from "react-icons/io";
+import { FaReact, FaGitAlt, FaJava } from "react-icons/fa";
+import { SiMui, SiJest, SiGradle } from "react-icons/si";
+import { TbBrandNextjs, TbBrandTypescript, TbBrandDjango, TbSql } from "react-icons/tb";
+import { BiLogoTailwindCss, BiLogoSpringBoot } from "react-icons/bi";
+import { RiFirebaseFill } from "react-icons/ri";
+
+const timelineData = [
+  {
+    date: 'August 2024 - Present',
+    title: 'TDP Analyst',
+    company: 'PNC',
+    description: (
+      <div>
+        <p className="mb-2">Technology Development Program</p>
+        <ul className="list-disc ml-5 space-y-2">
+          <li>Rotation 1 - Security Engineer<br /> Bullet Points coming soon..</li>
+        </ul>
+        <hr className="mt-2" />
+        <div className="flex flex-col mt-4">
+          <h2 className="font-bold mb-2">Skills</h2>
+          <div className="flex space-x-4">
+          <FaJava size={24}/>
+          <BiLogoSpringBoot size={24}/>
+          <SiGradle size={24}/>
+          <TbSql size={24}/>
+          <FaGitAlt size={24}/>
+          </div>
+          </div>
+      </div>
+    ),
+  },
+  {
+    date: 'March 2024 - August 2024',
+    title: 'Chief Technology Officer',
+    company: 'Clean Plate Innovations',
+    description: (
+      <div>
+        <p className="mb-2">AI Vision and Machine Learning company</p>
+        <ul className="list-disc ml-5 space-y-2">
+          <li><span className="font-bold">Leadership & Strategy: </span> Spearhead AI vision technology strategies, aligning with company goals, and mentor tech teams in innovation and collaboration.</li>
+          <li><span className="font-bold">Dual Role Leadership:</span> Serve as CTO and Lead Engineer, balancing strategic oversight with hands-on technical involvement in AI and software projects.</li>
+          <li><span className="font-bold">Scaling Teams: </span>Drive growth of engineering and AI teams, focusing on talent recruitment, skill development, and fostering an inclusive work environment.</li>
+          <li><span className="font-bold">Technical Excellence: </span>Oversee AI-driven vision systems' design and deployment, ensuring adherence to best practices and security protocols.</li>
+          <li><span className="font-bold">AI Vision Innovation: </span>Lead groundbreaking AI vision projects, integrating cutting-edge technologies into scalable, robust solutions.</li>
+        </ul>
+        <hr className="mt-2" />
+        <div className="flex flex-col mt-4">
+          <h2 className="font-bold mb-2">Skills</h2>
+          <div className="flex space-x-4">
+            <IoLogoJavascript size={24} />
+            <TbBrandTypescript size={24} />
+            <FaReact size={24} />
+            <TbBrandNextjs size={24} />
+            <BiLogoTailwindCss size={24} />
+            <RiFirebaseFill size={24} />
+            <FaGitAlt size={24}/>
+
+          </div>
+        </div>
+      </div>
+    )
+  },
+  {
+    date: 'March 2024 - April 2024',
+    title: 'Front-End Developer',
+    company: 'Greetings',
+    description: (
+      <div>
+        <p className="mb-2">Frontend Developer for a Networking Start-up</p>
+        <ul className="list-disc ml-5 space-y-2">
+          <li>Developed web applications using Next.js, leveraging SSR, SSG, and API routes.</li>
+          <li>Wrote clean, maintainable code, adhering to best practices for Next.js development.</li>
+          <li>Collaborated with cross-functional teams to create a cohesive user experience.</li>
+          <li>Optimized web applications for performance, compatibility, and responsiveness.</li>
+        </ul>
+        <hr className="mt-2" />
+        <div className="flex flex-col mt-4">
+          <h2 className="font-bold mb-2">Skills</h2>
+          <div className="flex space-x-4">
+            <IoLogoJavascript size={24} />
+            <TbBrandTypescript size={24} />
+            <FaReact size={24} />
+            <TbBrandDjango size={24} />
+            <TbBrandNextjs size={24} />
+            <BiLogoTailwindCss size={24} />
+            <SiJest size={24} />
+            <FaGitAlt size={24}/>
+          </div>
+        </div>
+      </div>
+    ),
+  },
+  {
+    date: 'May 2023 - August 2023',
+    title: 'Software Engineer',
+    company: 'PNC',
+    description: (
+      <div>
+        <p className="mb-2">Software Engineering Intern</p>
+        <ul className="list-disc ml-5 space-y-2">
+          <li>Designed web app wireframes to streamline UI and user flows.</li>
+          <li>Delivered timely projects in an agile environment with adaptable solutions.</li>
+          <li>Implemented novel React features for improved functionality and UX.</li>
+          <li>Collaborated with team to convert requirements into efficient code.</li>
+          <li>Exhibited strong software development principles including testing and version control.</li>
+          <li>Debugged complex issues for smooth functionality and optimal performance.</li>
+          <li>Prioritized high-quality code via meticulous reviews and coding standard adherence.</li>
+          <li>Integrated APIs for real-time access to company data.</li>
+        </ul>
+        <hr className="mt-2" />
+        <div className="flex flex-col mt-4">
+          <h2 className="font-bold mb-2">Skills</h2>
+          <div className="flex space-x-4">
+            <IoLogoJavascript size={24} />
+            <FaReact size={24} />
+            <TbBrandNextjs size={24} />
+            <BiLogoTailwindCss size={24} />
+            <RiFirebaseFill size={24} />
+            <SiMui size={24} />
+            <SiJest size={24} />
+            <FaGitAlt size={24}/>
+          </div>
+        </div>
+      </div>
+    )
+  },
+];
+
+const Experience = () => {
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  return (
+    <section id="experience" className="my-40">
+      <h1 className="text-3xl font-bold text-center mb-12">Work Experience</h1>
+      <div className="relative wrap overflow-hidden p-10 h-full">
+        <div className="absolute h-full border border-blue-500 left-1/2 transform -translate-x-1/2"></div>
+        {timelineData.map((item, index) => (
+          <div
+            key={index}
+            className={`mb-8 flex justify-between items-center w-full cursor-pointer ${
+              index % 2 === 0 ? "flex-row-reverse" : "flex-row"
+            }`}
+            onClick={() => setSelectedIndex(index)}
+          >
+            <div className="w-5/12"></div>
+            <div
+              className={`relative bg-blue-500 rounded-lg w-5/12 px-6 py-4 transition-shadow duration-300 ${
+                selectedIndex === index
+                  ? "shadow-2xl border-2 border-white"
+                  : "shadow-lg"
+              }`}
+            >
+              <h3 className="mb-2 font-bold text-white text-xl">
+                {item.title}
+              </h3>
+              <p className="text-sm leading-snug tracking-wide text-white">
+                {item.date}
+              </p>
+              <p className="text-sm leading-snug tracking-wide text-white">
+                {item.company}
+              </p>
+              <div className="mt-2 text-sm leading-snug tracking-wide text-white">
+                {item.description}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Experience;
